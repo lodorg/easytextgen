2f58cc13d9cb5ce35763a87921afa3a1e47258d2
Lodor <44918244+lodorg@users.noreply.github.com>
2022-05-09 16:43:33 +0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='easytextgen',
     version='1.1',
     description='API to work with transformer based text generation engines and creating prompts.',
-    packages=['easytextgen'],
+    packages=find_packages(),
     install_requires=[
         'openai',
         'attrs',
